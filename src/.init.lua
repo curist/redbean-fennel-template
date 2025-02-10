8d6f1e01cff6580de2f756fe29861c64635c86aa
curist <curist.cyc@gmail.com>
2025-02-10 09:15:26 +0800

    remove string:endswith
    use s:match directly

--- src/.init.lua
+++ src/.init.lua
@@ -6,10 +6,6 @@ local NavBar = require("NavBar")
 
 HidePath("/usr/share")
 
-function string:endswith(suffix)
-  return self:sub(-#suffix) == suffix
-end
-
 local function renderPage(pages)
   local Page = pages[GetMethod()] or require("NotFoundPage")
   Write(SakuraCSS)
@@ -19,14 +15,14 @@ end
 
 function OnHttpRequest()
   local p = GetPath()
-  if p:endswith(".fnl") then
+  if p:match(".fnl$") then
     local found, pages = pcall(require, p:gsub(".fnl$", ""))
     if found then
       renderPage(pages)
     else
       Route()
     end
-  elseif p:endswith("/") and path.exists("/zip" .. p .. "index.fnl") then
+  elseif p:match("/$") and path.exists("/zip" .. p .. "index.fnl") then
     local found, pages = pcall(require, p .. "index")
     if found then
       renderPage(pages)
